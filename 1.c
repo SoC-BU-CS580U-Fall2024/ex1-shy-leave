cc51a19c38240c8e0ebfb909d9de05d7a10fc800
shy-leave <wxue@binghamton.edu>
2025-12-01 00:08:45 -0500

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    printf("Letter G:\n");
+    printf(" #####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#  ###\n");
+    printf("#    #\n");
+    printf(" #####\n");
+
+    printf("\nLetter H:\n");
+    printf("#     #\n");
+    printf("#     #\n");
+    printf("#######\n");
+    printf("#     #\n");
+    printf("#     #\n");
+
+    return 0;
+}
